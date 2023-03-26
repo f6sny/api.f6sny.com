8a12970f5fa4e08ffa45d66060a62cbb6b7f9b19
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-03-26 04:21:02 +0300

    files to work

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8080),
-  url: env("URL", "http://localhost:8080"),
+  port: env.int('PORT', 80),
+  url: env("URL", ""),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', ''),
