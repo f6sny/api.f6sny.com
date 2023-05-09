f2cc358ad4db21ef7aac2e6faeb8029d28a9d777
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-05-09 11:57:35 +0300

    ..

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://api.example.com'),
+  url: env('PUBLIC_URL', 'https://api.example2.com'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'someSecretKey'),
