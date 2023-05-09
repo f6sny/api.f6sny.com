3ad2d7c121daed996fc17e7ee47a82ea9f8d9e63
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-05-09 13:49:46 +0300

    deploy for production

--- config/env/development/server.js
+++ config/env/development/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8080),
-  url: env("URL", "http://localhost:8080"),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', ''),
--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'https://api.example2.com'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'someSecretKey'),
