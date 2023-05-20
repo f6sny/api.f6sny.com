a7567ab977a75033f9e54d8ecec6dd18efa4ca60
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-05-20 11:09:05 +0300

    ..

--- config/env/development/server.js
+++ config/env/development/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'http://localhost'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', ''),
