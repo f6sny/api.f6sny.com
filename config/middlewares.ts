6d4a17ad97edf2f825675ce1342a7c4ba89c3ba6
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2025-03-03 03:58:54 +0300

    fix cdn

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,37 @@
-export default [
+export default ({ env }) => [
   'strapi::logger',
+  
   'strapi::cors',
   'strapi::body',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            `${env("CDN_URL")}`,
+            env("CDN_URL"),
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            `${env("CDN_URL")}`,
+            env("CDN_URL"),
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::session',
